de2b97b6fefa4f6e7be0d03c56fa51fc315e02a1
“JOA” <lorentejoaquin@icloud>
2025-12-13 12:18:19 +0100

    Fase 2.2: adaptación de la UT1 a estructura Angular y routing

--- liga-deportiva-angular/src/app/app-routing.module.ts
+++ liga-deportiva-angular/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
+import { JugadoresComponent } from './pages/jugadores/jugadores.component';
+import { ClasificacionesComponent } from './pages/clasificaciones/clasificaciones.component';
+import { ArbitrosComponent } from './pages/arbitros/arbitros.component';
+import { ResultadosComponent } from './pages/resultados/resultados.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'equipos', component: EquiposComponent },
+  { path: 'jugadores', component: JugadoresComponent },
+  { path: 'clasificaciones', component: ClasificacionesComponent },
+  { path: 'arbitros', component: ArbitrosComponent },
+  { path: 'resultados', component: ResultadosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- liga-deportiva-angular/src/app/app.module.ts
+++ liga-deportiva-angular/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
+import { JugadoresComponent } from './pages/jugadores/jugadores.component';
+import { ClasificacionesComponent } from './pages/clasificaciones/clasificaciones.component';
+import { ArbitrosComponent } from './pages/arbitros/arbitros.component';
+import { ResultadosComponent } from './pages/resultados/resultados.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    EquiposComponent,
+    JugadoresComponent,
+    ClasificacionesComponent,
+    ArbitrosComponent,
+    ResultadosComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
