d2685b0eef6cdf60fbc7c4e4658c6ac2d100c094
“JOA” <lorentejoaquin@icloud>
2025-12-16 17:24:00 +0100

    Contacto: formulario Angular y modal de inscripción controlado por componente

--- liga-deportiva-angular/src/app/pages/contacto/contacto.component.ts
+++ liga-deportiva-angular/src/app/pages/contacto/contacto.component.ts
@@ -7,27 +7,42 @@ import { Component } from '@angular/core';
 })
 export class ContactoComponent {
 
+  // ===== FORMULARIO DE CONTACTO =====
   contacto = {
     nombre: '',
     email: '',
     mensaje: ''
   };
 
+  // ===== FORMULARIO DE INSCRIPCIÓN =====
   inscripcion = {
     nombre: '',
     email: '',
     deporte: ''
   };
 
+  // ===== CONTROL MODAL =====
+  modalAbierto = false;
+
+  // ===== MÉTODOS =====
   enviarMensaje() {
     console.log('Mensaje enviado', this.contacto);
     alert('Mensaje enviado correctamente');
     this.contacto = { nombre: '', email: '', mensaje: '' };
   }
 
+  abrirModal() {
+    this.modalAbierto = true;
+  }
+
+  cerrarModal() {
+    this.modalAbierto = false;
+  }
+
   inscribirse() {
     console.log('Inscripción', this.inscripcion);
     alert('Inscripción realizada');
     this.inscripcion = { nombre: '', email: '', deporte: '' };
+    this.cerrarModal();
   }
 }
