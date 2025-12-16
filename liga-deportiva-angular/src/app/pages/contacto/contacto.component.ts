231ca2a051951d4ed7eee9b70087165c6e632f74
“JOA” <lorentejoaquin@icloud>
2025-12-16 17:18:39 +0100

    Contacto: adaptar página Bootstrap a componente Angular

--- liga-deportiva-angular/src/app/pages/contacto/contacto.component.ts
+++ liga-deportiva-angular/src/app/pages/contacto/contacto.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class ContactoComponent {
 
+  contacto = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+  };
+
+  inscripcion = {
+    nombre: '',
+    email: '',
+    deporte: ''
+  };
+
+  enviarMensaje() {
+    console.log('Mensaje enviado', this.contacto);
+    alert('Mensaje enviado correctamente');
+    this.contacto = { nombre: '', email: '', mensaje: '' };
+  }
+
+  inscribirse() {
+    console.log('Inscripción', this.inscripcion);
+    alert('Inscripción realizada');
+    this.inscripcion = { nombre: '', email: '', deporte: '' };
+  }
 }
